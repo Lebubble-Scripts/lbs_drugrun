7998ff94d1ce01f2368710ec17937ca0d05983a8
Jacob Roelofsz <87379740+jroelofsz@users.noreply.github.com>
2025-05-25 20:25:23 -0500

    (feat) added cooldown config

--- shared/config.lua
+++ shared/config.lua
@@ -5,9 +5,11 @@ Config.Notify = 'qb'        -- 'ox', 'qb'
 Config.Framework = 'qb'     -- 'qbx', 'qb',
 
 
--- Do not change this unless you know what you are doing
 if Config.Framework == 'qb' then
     QBCore = exports['qb-core']:GetCoreObject()
-elseif Config.Framework == 'qbx' then
-    qbx = exports.qbx_core
 end
+
+-- Set the cooldown time in seconds
+-- 60 * 5 = 300 seconds = 5 minutes
+-- 60 * 60 * 2 = 7200 seconds = 2 hours
+Config.Cooldown = 10
