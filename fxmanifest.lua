bd8e83a96c66462c90943bd2533f3b876af4b824
Jacob Roelofsz <87379740+jroelofsz@users.noreply.github.com>
2025-05-25 20:24:45 -0500

    (chore) add drugrun.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,32 +1,24 @@
-fx_version "cerulean"
-
-description "Boilerplate for FiveM resources"
-author "Lebubble Scripts"
-version '1.0.0'
-
+fx_version 'cerulean'
 lua54 'yes'
+game 'gta5'
 
-game "gta5"
-
--- Use below if you want to use a NUI interface
---ui_page 'web/build/index.html'
-
-shared_script  {
-  'shared/config.lua',
-  --'@ox_lib/init.lua'
+server_scripts {
+    'server/functions.lua',
+    'server/server.lua',
+    'server/callbacks.lua'
 }
 
--- Uncomment below if you will use oxmysql or ox-lib
---dependencies {
---  'ox_lib',
---  'oxmysql'
---}
-
-client_script "client/**/*"
-
-server_scripts {  
-  -- Uncomment below if you will use oxmysql
-  --'@oxmysql/lib/MySQL.lua',
-  "server/**/*"
+client_scripts {
+    'client/functions.lua',
+    'client/client.lua',
+    'client/drugrun.lua',
 }
 
+shared_script {
+    'shared/config.lua',
+    '@ox_lib/init.lua',
+}
+
+dependency {
+    'ox_lib'
+}
