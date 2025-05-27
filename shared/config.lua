c7c2b0d5d489d054b96d22062a660addd07de439
Jacob Roelofsz <87379740+jroelofsz@users.noreply.github.com>
2025-05-26 19:43:46 -0500

    (feat) moved run locations to config.lua for end-user support of adding runs

--- shared/config.lua
+++ shared/config.lua
@@ -14,3 +14,22 @@ end
 -- 60 * 5 = 300 seconds = 5 minutes
 -- 60 * 60 * 2 = 7200 seconds = 2 hours
 Config.Cooldown = 10
+
+-- Set different run locations here, for now we only do weed run
+---@param pickupCoords vector3 : the location where the truck will spawn for pickup
+---@param deliveryCoords vector3 : the location where the truck will deliver the weed boxes
+---@param propCoords vector3 : the location where the weed pallet will spawn
+Config.Locations = {
+    {
+        pickupCoords = vector3(1983.02, 3777.55, 32.18),
+        deliveryCoords = vector3(1677.92, 3281.74, 40.83),
+        propCoords = vector3(1974.14, 3766.81, 32.19),
+    },
+    --below is an example of how to add another run location
+    --the script will randomly select one of the locations from the list
+    --{
+    --    pickupCoords = vector3(2486.95, 3726.88, 43.92),
+    --    deliveryCoords = vector3(1677.92, 3281.74, 40.83),
+    --    propCoords = vector3(2486.95, 3726.88, 43.92),
+    --},
+}
