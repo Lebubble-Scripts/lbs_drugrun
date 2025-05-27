b714dbc51c7d8fae3de58771b2bdcb8177983d5c
Jacob Roelofsz <87379740+jroelofsz@users.noreply.github.com>
2025-05-26 19:42:37 -0500

    (feat) moved variables to central file for easier tracking

---
+++ client/variables.lua
@@ -0,0 +1,17 @@
+--client.lua
+boxObj = nil
+carryingAnimDict = 'anim@heists@box_carry@';
+carryingAnimName = 'idle';
+boxModel = 'hei_prop_heist_box';
+
+--drugrun.lua
+missionActive = false
+truck = nil
+pickupBlip = nil
+deliveryBlip = nil
+hasArrivedAtPickup = false
+boxesPickedUp = 0
+boxesToPickUp = 1
+
+weedPallet = "hei_prop_heist_weed_pallet"
+palletObj = nil
